a9cd98508ada91c4687cb70fd1957adae28a14c3
E.Z. Hart <hartez@gmail.com>
2017-03-29 09:14:20 -0600

    First run at removing async void image update methods
    Consistent error logging and IsLoading on Android,iOS,UWP
    
    Move error logging into image handlers for better messages
    
    Add demo of custom ImageRenderer error handling
    
    Update docs
    
    Make the test smaller so the results don't get pushed offscreen
    
    Fix namespace error

--- Xamarin.Forms.Platform.Android/Renderers/ImageRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/ImageRenderer.cs
@@ -4,7 +4,6 @@ using Android.Graphics;
 using Android.Views;
 using AImageView = Android.Widget.ImageView;
 using Xamarin.Forms.Internals;
-using static Xamarin.Forms.Platform.Android.ImageViewExtensions;
 
 namespace Xamarin.Forms.Platform.Android
 {
@@ -20,7 +19,6 @@ namespace Xamarin.Forms.Platform.Android
 
 		public ImageRenderer()
 		{
-            System.Diagnostics.Debug.WriteLine(">>>>> Old Image Renderer");
 			AutoPackage = false;
 		}
 
@@ -39,7 +37,7 @@ namespace Xamarin.Forms.Platform.Android
 			return new FormsImageView(Context);
 		}
 
-		protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+		protected override async void OnElementChanged(ElementChangedEventArgs<Image> e)
 		{
 			base.OnElementChanged(e);
 
@@ -50,18 +48,18 @@ namespace Xamarin.Forms.Platform.Android
 			}
 
 			_motionEventHelper.UpdateElement(e.NewElement);
-			
-			Control.UpdateBitmap(e.NewElement, e.OldElement);
+
+			await TryUpdateBitmap(e.OldElement);
 
 			UpdateAspect();
 		}
 
-		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		protected override async void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
 		{
 			base.OnElementPropertyChanged(sender, e);
 
 			if (e.PropertyName == Image.SourceProperty.PropertyName)
-				Control.UpdateBitmap(Element);
+				await TryUpdateBitmap();
 			else if (e.PropertyName == Image.AspectProperty.PropertyName)
 				UpdateAspect();
 		}
@@ -70,6 +68,77 @@ namespace Xamarin.Forms.Platform.Android
 		{
 			AImageView.ScaleType type = Element.Aspect.ToScaleType();
 			Control.SetScaleType(type);
+		}
+
+		protected virtual async Task TryUpdateBitmap(Image previous = null)
+		{
+			// By default we'll just catch and log any exceptions thrown by UpdateBitmap so they don't bring down
+			// the application; a custom renderer can override this method and handle exceptions from
+			// UpdateBitmap differently if it wants to
+
+			try
+			{
+				await UpdateBitmap(previous);
+			}
+			catch (Exception ex)
+			{
+				Log.Warning(nameof(ImageRenderer), "Error loading image: {0}", ex);
+			}
+			finally
+			{
+				((IImageController)Element).SetIsLoading(false);
+			}
+		}
+
+		protected async Task UpdateBitmap(Image previous = null)
+		{
+			if (Device.IsInvokeRequired)
+				throw new InvalidOperationException("Image Bitmap must not be updated from background thread");
+
+			if (previous != null && Equals(previous.Source, Element.Source))
+				return;
+
+			((IImageController)Element).SetIsLoading(true);
+
+			var formsImageView = Control as FormsImageView;
+			formsImageView?.SkipInvalidate();
+
+			Control.SetImageResource(global::Android.Resource.Color.Transparent);
+
+			ImageSource source = Element.Source;
+			Bitmap bitmap = null;
+			IImageSourceHandler handler;
+
+			if (source != null && (handler = Registrar.Registered.GetHandler<IImageSourceHandler>(source.GetType())) != null)
+			{
+				try
+				{
+					bitmap = await handler.LoadImageAsync(source, Context);
+				}
+				catch (TaskCanceledException)
+				{
+					((IImageController)Element).SetIsLoading(false);
+				}
+			}
+
+			if (Element == null || !Equals(Element.Source, source))
+			{
+				bitmap?.Dispose();
+				return;
+			}
+
+			if (!_isDisposed)
+			{
+				if (bitmap == null && source is FileImageSource)
+					Control.SetImageResource(ResourceManager.GetDrawableByName(((FileImageSource)source).File));
+				else
+					Control.SetImageBitmap(bitmap);
+
+				bitmap?.Dispose();
+
+				((IImageController)Element).SetIsLoading(false);
+				((IVisualElementController)Element).NativeSizeChanged();
+			}
 		}
 
         public override bool OnTouchEvent(MotionEvent e)
