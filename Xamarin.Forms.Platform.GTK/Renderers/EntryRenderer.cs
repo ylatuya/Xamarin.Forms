f03aa1a62fbc932bf99fbad3f4fe88c035cbe2ae
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-04-20 17:22:58 +0200

    Updated Entry renderer

--- Xamarin.Forms.Platform.GTK/Renderers/EntryRenderer.cs
+++ Xamarin.Forms.Platform.GTK/Renderers/EntryRenderer.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Gtk;
 using System.ComponentModel;
+using Xamarin.Forms.Platform.GTK.Extensions;
 
 namespace Xamarin.Forms.Platform.GTK.Renderers
 {
     public class EntryRenderer : ViewRenderer<Entry, Gtk.Entry>
     {
         private bool _disposed;
+
+        IElementController ElementController => Element;
+
+        IEntryController EntryController => Element;
 
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
@@ -15,11 +20,17 @@ namespace Xamarin.Forms.Platform.GTK.Renderers
             {
                 Gtk.Entry entry = new Gtk.Entry();
                 SetNativeControl(entry);
+
+                entry.Changed += OnChanged;
+                entry.Focused += OnFocused;
+                entry.EditingDone += OnEditingDone;
             }
 
             if (e.NewElement != null)
             {
                 UpdateText();
+                UpdateColor();
+                UpdateAlignment();
             }
         }
 
@@ -27,6 +38,14 @@ namespace Xamarin.Forms.Platform.GTK.Renderers
         {
             if (e.PropertyName == Entry.TextProperty.PropertyName)
                 UpdateText();
+            else if (e.PropertyName == Entry.TextColorProperty.PropertyName)
+                UpdateColor();
+            else if (e.PropertyName == Entry.HorizontalTextAlignmentProperty.PropertyName)
+                UpdateAlignment();
+            else if (e.PropertyName == VisualElement.IsEnabledProperty.PropertyName)
+            {
+                UpdateColor();
+            }
 
             base.OnElementPropertyChanged(sender, e);
         }
@@ -36,6 +55,12 @@ namespace Xamarin.Forms.Platform.GTK.Renderers
             if (disposing && !_disposed)
             {
                 _disposed = true;
+                if (Control != null)
+                {
+                    Control.Changed -= OnChanged;
+                    Control.Focused -= OnFocused;
+                    Control.EditingDone -= OnEditingDone;
+                }
             }
 
             base.Dispose(disposing);
@@ -45,6 +70,49 @@ namespace Xamarin.Forms.Platform.GTK.Renderers
         {
             if (Control.Text != Element.Text)
                 Control.Text = Element.Text ?? string.Empty;
+        }
+
+        private void UpdateColor()
+        {
+            var textColor = Element.TextColor;
+
+            Control.ModifyFg(StateType.Normal, textColor.ToGtkColor());
+        }
+
+        private void UpdateAlignment()
+        {
+            var hAlignmentValue = GetAlignmentValue(Element.HorizontalTextAlignment);
+
+            Control.Alignment = hAlignmentValue;
+        }
+
+        private void OnChanged(object sender, System.EventArgs e)
+        {
+            ElementController.SetValueFromRenderer(Entry.TextProperty, Control.Text);
+        }
+
+        private void OnFocused(object o, FocusedArgs args)
+        {
+            ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, true);
+        }
+
+        private void OnEditingDone(object sender, System.EventArgs e)
+        {
+            ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, false);
+            EntryController?.SendCompleted();
+        }
+
+        private static float GetAlignmentValue(TextAlignment alignment)
+        {
+            switch (alignment)
+            {
+                case TextAlignment.Start:
+                    return 0f;
+                case TextAlignment.End:
+                    return 1f;
+                default:
+                    return 0.5f;
+            }
         }
     }
 }
