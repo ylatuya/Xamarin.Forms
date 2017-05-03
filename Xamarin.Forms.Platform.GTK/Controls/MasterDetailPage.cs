a2d0c9b569f607ce1d60461ebd3e9ea0558b5b43
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-05-03 17:03:12 +0200

    Initial and basic MasterDetailPage Renderer

---
+++ Xamarin.Forms.Platform.GTK/Controls/MasterDetailPage.cs
@@ -0,0 +1,191 @@
+﻿using Gtk;
+using System;
+using System.Linq;
+using Xamarin.Forms.Platform.GTK.Animations;
+
+namespace Xamarin.Forms.Platform.GTK.Controls
+{
+    public enum MasterBehaviorType
+    {
+        Default = 0,
+        Popover,
+        Split
+    }
+
+    public class MasterDetailPage : VBox
+    {
+        private MasterBehaviorType _masterBehaviorType;
+        private bool _isPresented;
+        private Widget _master;
+        private Widget _detail;
+        private HBox _rootPanel;
+        private VBox _masterContent;
+        private VBox _masterContainer;
+
+        private System.Action RefreshPresented;
+
+        public MasterBehaviorType MasterBehaviorType
+        {
+            get
+            {
+                return _masterBehaviorType;
+            }
+            set
+            {
+                if (_masterBehaviorType != value)
+                {
+                    _masterBehaviorType = value;
+
+                    RefreshBehavior();
+                }
+            }
+        }
+
+        public bool IsPresented
+        {
+            get
+            {
+                return _isPresented;
+            }
+            set
+            {
+                if (_isPresented != value)
+                {
+                    _isPresented = value;
+
+                    RefreshPresented();
+                }
+            }
+        }
+
+        public Widget Detail
+        {
+            get
+            {
+                return _detail;
+            }
+            set
+            {
+                if (_detail != value)
+                {
+                    RefreshDetail(value);
+                }
+            }
+        }
+
+        public Widget Master
+        {
+            get
+            {
+                return _master;
+            }
+            set
+            {
+                if (_master != value)
+                {
+                    _master = value;
+                    RefreshMaster();
+                }
+            }
+        }
+
+        public MasterDetailPage()
+        {
+            RefreshBehavior();
+            RefreshPresented();
+        }
+
+        private void RefreshBehavior()
+        {
+            switch (_masterBehaviorType)
+            {
+                case MasterBehaviorType.Popover:
+
+                    RefreshPresented = RefreshPopoverPresented;
+
+                    break;
+                case MasterBehaviorType.Default:
+                case MasterBehaviorType.Split:
+                default:
+                    _rootPanel = new HBox();
+                    _masterContent = new VBox();
+                    var masterHeader = new HBox();
+
+                    var menuButton = new Gtk.Button();
+                    menuButton.Name = "menuButton";
+                    menuButton.Label = null;
+                    menuButton.CanFocus = false;
+                    var image = new Gtk.Image(new Gdk.Pixbuf("./Resources/hamburger.png"));
+                    menuButton.Image = image;
+                    menuButton.Clicked += OnMenuButtonClicked;
+
+                    masterHeader.PackStart(menuButton, false, false, 0);
+                    _masterContent.PackStart(masterHeader, false, false, 0);
+
+                    _masterContainer = new VBox();
+                    _masterContent.PackEnd(_masterContainer, true, true, 3);
+
+                    _rootPanel.PackStart(_masterContent, false, false, 0);
+
+                    Add(_rootPanel);
+                    RefreshPresented = RefreshSplitPresented;
+                    break;
+            }
+        }
+
+        private void OnMenuButtonClicked(object sender, EventArgs e)
+        {
+            IsPresented = !IsPresented;
+        }
+
+        private void RefreshPopoverPresented()
+        {
+
+        }
+
+        private async void RefreshSplitPresented()
+        {
+            _masterContainer.Visible = _isPresented;
+
+            var from = (_isPresented) ? 0 : 200;
+            var to = (_isPresented) ? 200 : 0;
+
+            await new FloatAnimation(from, to, TimeSpan.FromSeconds(0.5), true, (f) =>
+            {
+                Gtk.Application.Invoke(delegate
+                {
+                    _masterContent.WidthRequest = (int)f;
+                });
+            }).Run();
+        }
+
+        private void RefreshMaster()
+        {
+            if (_masterContainer.Children.Length > 0)
+            {
+                _masterContainer.Remove(_masterContainer.Children.First());
+            }
+
+            _masterContainer.Add(_master);
+
+            RefreshPresented();
+        }
+
+        private void RefreshDetail(Widget newDetail)
+        {
+            if (_detail != null)
+            {
+                _rootPanel.Remove(newDetail);
+            }
+
+            _detail = newDetail;
+
+            _rootPanel.PackEnd(_detail, true, true, 0);
+        }
+
+        private void RefreshPopoverDetail()
+        {
+
+        }
+    }
+}
---
+++ Xamarin.Forms.Platform.GTK/Renderers/MasterDetailPageRenderer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel;
+using Container = Gtk.EventBox;
+
+namespace Xamarin.Forms.Platform.GTK.Renderers
+{
+    public class MasterDetailPageRenderer : Container, IVisualElementRenderer
+    {
+        private bool _disposed;
+        private MasterDetailPage _masterDetailPage;
+
+        IPageController PageController => Element as IPageController;
+
+        public Controls.MasterDetailPage Control { get; private set; }
+
+        public Container Container => this;
+
+        public VisualElement Element { get; private set; }
+
+        public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
+
+        protected MasterDetailPage MasterDetailPage => _masterDetailPage ?? (_masterDetailPage = (MasterDetailPage)Element);
+
+        public SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
+        {
+            var result = new Size(
+                 widthConstraint,
+                heightConstraint);
+
+            return new SizeRequest(result);
+        }
+
+        public void SetElement(VisualElement element)
+        {
+            var oldElement = Element;
+
+            Element = element;
+
+            UpdateMasterDetail();
+
+            OnElementChanged(new VisualElementChangedEventArgs(oldElement, element));
+        }
+
+        public void SetElementSize(Size size)
+        {
+            Element.Layout(new Rectangle(Element.X, Element.Y, size.Width, size.Height));
+        }
+
+        public override void Dispose()
+        {
+            if (!_disposed)
+            {
+                PageController?.SendDisappearing();
+
+                if (Element != null)
+                {
+                    Element.PropertyChanged -= HandlePropertyChanged;
+                    Element = null;
+                }
+
+                _disposed = true;
+            }
+
+            base.Dispose();
+        }
+
+        protected virtual void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            if (e.OldElement != null)
+                e.OldElement.PropertyChanged -= HandlePropertyChanged;
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    Control = new Controls.MasterDetailPage();
+                    Add(Control);
+
+                    UpdateMasterDetail();
+                    UpdateIsPresented();
+                }
+
+                e.NewElement.PropertyChanged += HandlePropertyChanged;
+            }
+
+            ElementChanged?.Invoke(this, e);
+        }
+
+        private void HandlePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("Master", StringComparison.CurrentCultureIgnoreCase) || e.PropertyName.Equals("Detail", StringComparison.CurrentCultureIgnoreCase))
+                UpdateMasterDetail();
+            else if (e.PropertyName == MasterDetailPage.IsPresentedProperty.PropertyName || e.PropertyName == MasterDetailPage.MasterBehaviorProperty.PropertyName)
+                UpdateIsPresented();
+        }
+
+        private void UpdateMasterDetail()
+        {
+            if (Platform.GetRenderer(MasterDetailPage.Master) == null)
+                Platform.SetRenderer(MasterDetailPage.Master, Platform.CreateRenderer(MasterDetailPage.Master));
+            if (Platform.GetRenderer(MasterDetailPage.Detail) == null)
+                Platform.SetRenderer(MasterDetailPage.Detail, Platform.CreateRenderer(MasterDetailPage.Detail));
+
+            if (Control != null)
+            {
+                Control.Master = Platform.GetRenderer(MasterDetailPage.Master).Container;
+                Control.Detail = Platform.GetRenderer(MasterDetailPage.Detail).Container;
+            }
+        }
+
+        private void UpdateIsPresented()
+        {
+            if (Control != null)
+            {
+                Control.IsPresented = MasterDetailPage.IsPresented;
+            }
+        }
+    }
+}
