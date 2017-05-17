9dd9fd22bc2d2bdce998953060565ec340077ba7
Sergio Escalada <sescalada@plainconcepts.com>
2017-05-17 12:34:10 +0200

    Removing time widgets from datepicker (only date selection needed), adding date ranges and also changing how picker window is displayed

--- Xamarin.Forms.Platform.GTK/Controls/DatePicker.cs
+++ Xamarin.Forms.Platform.GTK/Controls/DatePicker.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Gtk;
+using System;
 using System.Linq;
-using Gtk;
 
 namespace Xamarin.Forms.Platform.GTK.Controls
 {
@@ -25,38 +25,58 @@ namespace Xamarin.Forms.Platform.GTK.Controls
     public partial class DatePickerWindow : Window
     {
         private VBox _datebox;
-        private Calendar _calendar;
-        private HBox _timebox;
-        private Gtk.Label _labelHour;
-        private SpinButton _txtHour;
-        private Gtk.Label _labelMin;
-        private SpinButton _txtMin;
-        private Gtk.Label _labelSec;
-        private SpinButton _txtSec;
+        private RangeCalendar _calendar;
+
+        public DatePickerWindow()
+            : base(WindowType.Popup)
+        {
+            BuildDatePickerWindow();
+            Helpers.GrabHelper.GrabWindow(this);
+            SelectedDate = DateTime.Now;
+        }
+
+        public DateTime SelectedDate
+        {
+            get
+            {
+                return _calendar.Date;
+            }
+
+            set
+            {
+                _calendar.Date = value;
+            }
+        }
+
+        public DateTime MinimumDate
+        {
+            get
+            {
+                return _calendar.MinimumDate;
+            }
+
+            set
+            {
+                _calendar.MinimumDate = value;
+            }
+        }
+
+        public DateTime MaximumDate
+        {
+            get
+            {
+                return _calendar.MaximumDate;
+            }
+
+            set
+            {
+                _calendar.MaximumDate = value;
+            }
+        }
 
         public delegate void DateEventHandler(object sender, DateEventArgs args);
 
-        public event DateEventHandler OnChange = null;
-
-        public DatePickerWindow(int x, int y, DateTime defDate, DateEventHandler handler) : base(WindowType.Popup)
-        {
-            BuildDatePickerWindow();
-            Move(x, y);
-            OnChange = handler;
-            Helpers.GrabHelper.GrabWindow(this);
-
-            _txtHour.Value = defDate.Hour;
-            _txtMin.Value = defDate.Minute;
-            _txtSec.Value = defDate.Second;
-            _calendar.Date = defDate;
-
-            RefreshDate();
-        }
-
-        public static void ShowMe(int x, int y, DateTime defDate, DateEventHandler handler)
-        {
-            new DatePickerWindow(x, y, defDate, handler);
-        }
+        public event DateEventHandler OnDateTimeChanged;
 
         protected override bool OnExposeEvent(Gdk.EventExpose args)
         {
@@ -75,78 +95,19 @@ namespace Xamarin.Forms.Platform.GTK.Controls
             Close();
         }
 
-        private void Close()
-        {
-            Helpers.GrabHelper.RemoveGrab(this);
-            this.Destroy();
-        }
-
-        protected virtual void OnCalendar3ButtonPressEvent(object o, ButtonPressEventArgs args)
+        protected virtual void OnCalendarButtonPressEvent(object o, ButtonPressEventArgs args)
         {
             args.RetVal = true;
         }
 
-        private void RefreshDate()
+        protected virtual void OnCalendarDaySelected(object sender, EventArgs e)
         {
-            OnChange?.Invoke(this, new DateEventArgs(CurrentDate));
+            OnDateTimeChanged?.Invoke(this, new DateEventArgs(SelectedDate));
         }
 
-        protected virtual void OnTxtHourValueChanged(object sender, EventArgs e)
+        protected virtual void OnCalendarDaySelectedDoubleClick(object sender, EventArgs e)
         {
-            if (_txtHour.Value == 24) _txtHour.Value = 0;
-            RefreshDate();
-        }
-
-        protected virtual void OnTxtMinValueChanged(object sender, EventArgs e)
-        {
-            if (_txtMin.Value == 60) _txtMin.Value = 0;
-            RefreshDate();
-        }
-
-        protected virtual void OnTxtSecValueChanged(object sender, EventArgs e)
-        {
-            if (_txtSec.Value == 60) _txtSec.Value = 0;
-            RefreshDate();
-        }
-
-        protected virtual void OnTxtHourButtonPressEvent(object o, ButtonPressEventArgs args)
-        {
-            args.RetVal = true;
-        }
-
-        protected virtual void OnTxtMinButtonPressEvent(object o, ButtonPressEventArgs args)
-        {
-            args.RetVal = true;
-        }
-
-        protected virtual void OnTxtSecButtonPressEvent(object o, ButtonPressEventArgs args)
-        {
-            args.RetVal = true;
-        }
-
-        protected virtual void OnCalendar4ButtonPressEvent(object o, ButtonPressEventArgs args)
-        {
-            args.RetVal = true;
-        }
-
-
-        public DateTime CurrentDate
-        {
-            get
-            {
-                DateTime d = _calendar.Date;
-                return new DateTime(d.Year, d.Month, d.Day, (int)_txtHour.Value, (int)_txtMin.Value, (int)_txtSec.Value);
-            }
-        }
-
-        protected virtual void OnCCalendarDaySelected(object sender, EventArgs e)
-        {
-            OnChange?.Invoke(this, new DateEventArgs(CurrentDate));
-        }
-
-        protected virtual void OnCCalendarDaySelectedDoubleClick(object sender, EventArgs e)
-        {
-            OnChange?.Invoke(this, new DateEventArgs(CurrentDate));
+            OnDateTimeChanged?.Invoke(this, new DateEventArgs(SelectedDate));
             Close();
         }
 
@@ -167,74 +128,12 @@ namespace Xamarin.Forms.Platform.GTK.Controls
             _datebox.Spacing = 6;
             _datebox.BorderWidth = 3;
 
-            _calendar = new Calendar();
+            _calendar = new RangeCalendar();
             _calendar.CanFocus = true;
             _calendar.DisplayOptions = CalendarDisplayOptions.ShowHeading;
             _datebox.Add(_calendar);
             Box.BoxChild dateBoxChild = ((Box.BoxChild)(_datebox[_calendar]));
             dateBoxChild.Position = 0;
-
-            _timebox = new HBox();
-            _timebox.Spacing = 6;
-
-            _labelHour = new Gtk.Label();
-            _labelHour.LabelProp = "H:";
-            _timebox.Add(_labelHour);
-            Box.BoxChild labelHourBoxChild = ((Box.BoxChild)(_timebox[_labelHour]));
-            labelHourBoxChild.Position = 0;
-            labelHourBoxChild.Expand = false;
-            labelHourBoxChild.Fill = false;
-
-            _txtHour = new SpinButton(0D, 24D, 1D);
-            _txtHour.CanFocus = true;
-            _txtHour.Adjustment.PageIncrement = 1D;
-            _txtHour.ClimbRate = 1D;
-            _txtHour.Numeric = true;
-            _timebox.Add(_txtHour);
-            Box.BoxChild txtHourBoxChild = ((Box.BoxChild)(_timebox[_txtHour]));
-            txtHourBoxChild.Position = 1;
-            txtHourBoxChild.Expand = false;
-            txtHourBoxChild.Fill = false;
-
-            _labelMin = new Gtk.Label();
-            _labelMin.LabelProp = "M:";
-            _timebox.Add(_labelMin);
-            Box.BoxChild labelMinBoxChild = ((Box.BoxChild)(_timebox[_labelMin]));
-            labelMinBoxChild.Position = 2;
-            labelMinBoxChild.Expand = false;
-            labelMinBoxChild.Fill = false;
-
-            _txtMin = new SpinButton(0D, 60D, 1D);
-            _txtMin.CanFocus = true;
-            _txtMin.Adjustment.PageIncrement = 10D;
-            _txtMin.ClimbRate = 1D;
-            _txtMin.Numeric = true;
-            _timebox.Add(_txtMin);
-            Box.BoxChild txtMinBoxChild = ((Box.BoxChild)(_timebox[_txtMin]));
-            txtMinBoxChild.Position = 3;
-            txtMinBoxChild.Expand = false;
-            txtMinBoxChild.Fill = false;
-
-            _labelSec = new Gtk.Label();
-            _labelSec.LabelProp = "S:";
-            _timebox.Add(_labelSec);
-            Box.BoxChild labelSecBoxChild = ((Box.BoxChild)(_timebox[_labelSec]));
-            labelSecBoxChild.Position = 4;
-            labelSecBoxChild.Expand = false;
-            labelSecBoxChild.Fill = false;
-
-            _txtSec = new SpinButton(0D, 60D, 1D);
-            _txtSec.CanFocus = true;
-            _txtSec.Adjustment.PageIncrement = 10D;
-            _txtSec.ClimbRate = 1D;
-            _txtSec.Numeric = true;
-            _timebox.Add(_txtSec);
-            Box.BoxChild timeboxBoxChild = ((Box.BoxChild)(_timebox[_txtSec]));
-            timeboxBoxChild.Position = 5;
-            timeboxBoxChild.Expand = false;
-            timeboxBoxChild.Fill = false;
-
-            _datebox.Add(_timebox);
 
             Add(_datebox);
 
@@ -246,15 +145,81 @@ namespace Xamarin.Forms.Platform.GTK.Controls
             Show();
 
             ButtonPressEvent += new ButtonPressEventHandler(OnButtonPressEvent);
-            _calendar.ButtonPressEvent += new ButtonPressEventHandler(OnCalendar4ButtonPressEvent);
-            _calendar.DaySelected += new EventHandler(OnCCalendarDaySelected);
-            _calendar.DaySelectedDoubleClick += new EventHandler(OnCCalendarDaySelectedDoubleClick);
-            _txtHour.ValueChanged += new EventHandler(OnTxtHourValueChanged);
-            _txtHour.ButtonPressEvent += new ButtonPressEventHandler(OnTxtHourButtonPressEvent);
-            _txtMin.ValueChanged += new EventHandler(OnTxtMinValueChanged);
-            _txtMin.ButtonPressEvent += new ButtonPressEventHandler(OnTxtMinButtonPressEvent);
-            _txtSec.ValueChanged += new EventHandler(OnTxtSecValueChanged);
-            _txtSec.ButtonPressEvent += new ButtonPressEventHandler(OnTxtSecButtonPressEvent);
+            _calendar.ButtonPressEvent += new ButtonPressEventHandler(OnCalendarButtonPressEvent);
+            _calendar.DaySelected += new EventHandler(OnCalendarDaySelected);
+            _calendar.DaySelectedDoubleClick += new EventHandler(OnCalendarDaySelectedDoubleClick);
+        }
+
+        private void Close()
+        {
+            Helpers.GrabHelper.RemoveGrab(this);
+            this.Destroy();
+        }
+
+        private void NotifyDateChanged()
+        {
+            OnDateTimeChanged?.Invoke(this, new DateEventArgs(SelectedDate));
+        }
+
+        class RangeCalendar : Calendar
+        {
+            private DateTime _minimumDate;
+            private DateTime _maximumDate;
+
+            public RangeCalendar()
+            {
+                _minimumDate = new DateTime(1900, 1, 1);
+                _maximumDate = new DateTime(2199, 1, 1);
+            }
+
+            public DateTime MinimumDate
+            {
+                get
+                {
+                    return _minimumDate;
+                }
+
+                set
+                {
+                    if (MaximumDate < value)
+                    {
+                        throw new InvalidOperationException($"{nameof(MinimumDate)} must be lower than {nameof(MaximumDate)}");
+                    }
+
+                    _minimumDate = value;
+                }
+            }
+
+            public DateTime MaximumDate
+            {
+                get
+                {
+                    return _maximumDate;
+                }
+
+                set
+                {
+                    if (MinimumDate > value)
+                    {
+                        throw new InvalidOperationException($"{nameof(MaximumDate)} must be greater than {nameof(MinimumDate)}");
+                    }
+
+                    _maximumDate = value;
+                }
+            }
+
+            protected override void OnDaySelected()
+            {
+                if (Date < MinimumDate)
+                {
+                    Date = MinimumDate;
+                }
+
+                if (Date > MaximumDate)
+                {
+                    Date = MaximumDate;
+                }
+            }
         }
     }
 
@@ -334,7 +299,7 @@ namespace Xamarin.Forms.Platform.GTK.Controls
                 _comboBox.Color = _color;
             }
         }
-        
+
         public string DateFormat
         {
             get
@@ -353,15 +318,16 @@ namespace Xamarin.Forms.Platform.GTK.Controls
             int x = 0;
             int y = 0;
 
-            var window = RootWindow.Children.FirstOrDefault(c => c.IsVisible);
-            if (window != null)
-            {
-                window.GetPosition(out x, out y);
-            }
+            GdkWindow.GetOrigin(out x, out y);
+            y += Allocation.Height;
 
-            x += Allocation.Left;
-            y += Allocation.Top + Allocation.Height;
-            DatePickerWindow.ShowMe(x, y, CurrentDate, OnPopupDateChanged);
+            var picker = new DatePickerWindow();
+            picker.Move(x, y);
+            picker.SelectedDate = CurrentDate;
+            picker.MinimumDate = _minDate;
+            picker.MaximumDate = _maxDate;
+            picker.OnDateTimeChanged += OnPopupDateChanged;
+            picker.Destroyed += OnPickerClosed;
         }
 
         private void OnPopupDateChanged(object sender, DateEventArgs args)
@@ -412,6 +378,16 @@ namespace Xamarin.Forms.Platform.GTK.Controls
         private void OnEntryFocused(object sender, EventArgs e)
         {
             ShowPickerWindow();
+        }
+
+        private void OnPickerClosed(object sender, EventArgs e)
+        {
+            var window = sender as DatePickerWindow;
+
+            if (window != null)
+            {
+                Remove(window);
+            }
         }
     }
 }
