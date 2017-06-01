04d4b2ecb0b1bee7e422974720892c2df0de2478
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-06-01 13:10:29 +0200

    Added custom WebView control Updated WebViewRenderer

---
+++ Xamarin.Forms.Platform.GTK/Controls/WebView.cs
@@ -0,0 +1,50 @@
+﻿using Gtk;
+using WebKit;
+
+namespace Xamarin.Forms.Platform.GTK.Controls
+{
+    public class WebView : EventBox
+    {
+        private VBox _vbox = null;
+        private WebKit.WebView _webview = null;
+
+        public WebView()
+        {
+            BuildWebView();
+        }
+
+        public WebKit.WebView Browser
+        {
+            get { return _webview; }
+        }
+
+        public void Open(string uri)
+        {
+            _webview.Open(uri);
+        }
+
+        public void LoadString(string html, string baseUrl)
+        {
+            _webview.LoadString(html, string.Empty, string.Empty, baseUrl);
+        }
+
+        private void BuildWebView()
+        {
+            CreateWebView();
+
+            ScrolledWindow scroll = new ScrolledWindow();
+            scroll.Add(_webview);
+            _vbox = new VBox(false, 1);
+            _vbox.PackStart(scroll);
+
+            Add(_vbox);
+            ShowAll();
+        }
+
+        private void CreateWebView()
+        {
+            _webview = new WebKit.WebView();
+            _webview.Editable = false;
+        }
+    }
+}
