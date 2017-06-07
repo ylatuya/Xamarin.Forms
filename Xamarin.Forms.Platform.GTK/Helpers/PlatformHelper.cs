9d2a6126c2f3636552e37cbea662e13023bfd672
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-06-07 16:13:15 +0200

    Added PlatformHelper

---
+++ Xamarin.Forms.Platform.GTK/Helpers/PlatformHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Xamarin.Forms.Platform.GTK.Helpers
+{
+    public enum GTKPlatform
+    {
+        Linux,
+        MacOS,
+        Windows
+    }
+
+    public class PlatformHelper
+    {
+        public static GTKPlatform GetGTKPlatform()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            PlatformID pid = os.Platform;
+
+            switch (pid)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    return GTKPlatform.Windows;
+                case PlatformID.Unix:
+                    return GTKPlatform.Linux;
+                case PlatformID.MacOSX:
+                    return GTKPlatform.MacOS;
+                default:
+                    return GTKPlatform.Windows;
+
+            }
+        }
+    }
+}
