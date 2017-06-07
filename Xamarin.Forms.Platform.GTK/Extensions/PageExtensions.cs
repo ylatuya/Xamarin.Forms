54f7fb23d508422ce2349b27c94dfde4a71677e3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-06-07 16:20:55 +0200

    Created PageExtensions

---
+++ Xamarin.Forms.Platform.GTK/Extensions/PageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xamarin.Forms.Platform.GTK.Extensions
+{
+    public static class PageExtensions
+    {
+        public static Gtk.EventBox CreateContainer(this VisualElement view)
+        {
+            if (!Forms.IsInitialized)
+                throw new InvalidOperationException("call Forms.Init() before this");
+
+            throw new NotImplementedException();
+        }
+    }
+}
