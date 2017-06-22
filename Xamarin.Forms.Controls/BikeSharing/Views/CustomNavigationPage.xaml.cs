e603344bd9e11329dc5994ea88a89538a59fbe92
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-06-22 13:31:03 +0200

    Added custom back button icon

--- Xamarin.Forms.Controls/BikeSharing/Views/CustomNavigationPage.xaml.cs
+++ Xamarin.Forms.Controls/BikeSharing/Views/CustomNavigationPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Xamarin.Forms.Controls
+﻿using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.GTKSpecific;
+
+namespace Xamarin.Forms.Controls
 {
     public partial class CustomNavigationPage : NavigationPage
     {
@@ -10,6 +13,7 @@
         public CustomNavigationPage(Page root) : base(root)
         {
             InitializeComponent();
+            this.On<GTK>().SetBackButtonIcon("gtk_back_button.png");
         }
     }
 }
