86ce9443015d9173cc19ab246d19ee2e3c51f0bb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-07-09 13:40:31 +0200

    Added more logic to FontButton and ToggleButton

--- Xamarin.Forms.Controls.Core.GTK/Renderers/FontButtonRenderer.cs
+++ Xamarin.Forms.Controls.Core.GTK/Renderers/FontButtonRenderer.cs
@@ -1,4 +1,5 @@
-﻿using GtkToolkit.Controls;
+﻿using System.ComponentModel;
+using GtkToolkit.Controls;
 using GtkToolkit.GTK.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.GTK;
@@ -8,6 +9,7 @@ namespace GtkToolkit.GTK.Renderers
 {
     public class FontButtonRenderer : ViewRenderer<FontButton, Gtk.FontButton>
     {
+        private bool _disposed;
         private Gtk.FontButton _fontButton;
 
         protected override void OnElementChanged(ElementChangedEventArgs<FontButton> e)
@@ -15,14 +17,97 @@ namespace GtkToolkit.GTK.Renderers
             if (Control == null)
             {
                 _fontButton = new Gtk.FontButton();
+                _fontButton.UseFont = true;
+                _fontButton.UseSize = true;
+
+                _fontButton.FontSet += OnFontSet;
 
                 Add(_fontButton);
+
                 _fontButton.ShowAll();
 
                 SetNativeControl(_fontButton);
             }
 
+            if (e.NewElement != null)
+            {
+                UpdateFontName();
+                UpdateTitle();
+                UpdateShowSize();
+                UpdateShowStyle();
+            }
+
             base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == FontButton.FontNameProperty.PropertyName)
+                UpdateFontName();
+            else if (e.PropertyName == FontButton.TitleProperty.PropertyName)
+                UpdateTitle();
+            else if (e.PropertyName == FontButton.ShowSizeProperty.PropertyName)
+                UpdateShowSize();
+            else if (e.PropertyName == FontButton.ShowStyleProperty.PropertyName)
+                UpdateShowStyle();
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
+
+                if (Control != null)
+                {
+                    Control.FontSet -= OnFontSet;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void UpdateFontName()
+        {
+            if (_fontButton != null)
+            {
+                var fontName = Element.FontName;
+                _fontButton.FontName = fontName;
+            }
+        }
+
+        private void UpdateTitle()
+        {
+            if (_fontButton != null)
+            {
+                var title = Element.Title;
+                _fontButton.Title = title;
+            }
+        }
+
+        private void UpdateShowSize()
+        {
+            if (_fontButton != null)
+            {
+                var showSize = Element.ShowSize;
+                _fontButton.ShowSize = showSize;
+            }
+        }
+
+        private void UpdateShowStyle()
+        {
+            if (_fontButton != null)
+            {
+                var showStyle = Element.ShowStyle;
+                _fontButton.ShowStyle = showStyle;
+            }
+        }
+
+        private void OnFontSet(object sender, System.EventArgs e)
+        {
+            Element.SendFontChanged();
         }
     }
 }
--- Xamarin.Forms.Controls.Core.GTK/Renderers/ToggleButtonRenderer.cs
+++ Xamarin.Forms.Controls.Core.GTK/Renderers/ToggleButtonRenderer.cs
@@ -1,4 +1,5 @@
-﻿using GtkToolkit.Controls;
+﻿using System.ComponentModel;
+using GtkToolkit.Controls;
 using GtkToolkit.GTK.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.GTK;
@@ -8,21 +9,67 @@ namespace GtkToolkit.GTK.Renderers
 {
     public class ToggleButtonRenderer : ViewRenderer<ToggleButton, Gtk.ToggleButton>
     {
+        private bool _disposed;
         private Gtk.ToggleButton _toggleButton;
 
         protected override void OnElementChanged(ElementChangedEventArgs<ToggleButton> e)
         {
             if (Control == null)
             {
-                _toggleButton = new Gtk.ToggleButton();
+                _toggleButton = new Gtk.ToggleButton(Element.Text);
+                _toggleButton.Inconsistent = false;
+
+                _toggleButton.Toggled += OnToggled;
 
                 Add(_toggleButton);
+
                 _toggleButton.ShowAll();
 
                 SetNativeControl(_toggleButton);
             }
 
+            if (e.NewElement != null)
+            {
+                UpdateIsToggled();
+            }
+
             base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == StatusBar.HasResizeGripProperty.PropertyName)
+                UpdateIsToggled();
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
+
+                if (Control != null)
+                {
+                    Control.Toggled -= OnToggled;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void UpdateIsToggled()
+        {
+            if (_toggleButton != null)
+            {
+                _toggleButton.Active = Element.IsToggled;
+            }
+        }
+
+        private void OnToggled(object sender, System.EventArgs e)
+        {
+            Element.SendIsToggledChanged();
         }
     }
 }
