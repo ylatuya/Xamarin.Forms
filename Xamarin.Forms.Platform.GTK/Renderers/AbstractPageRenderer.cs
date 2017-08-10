d5cc3316fa11e1245646ae0830c70b0ea4d1cc57
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-08-10 08:34:20 +0200

    Fixed PageSize with Toolbar.

--- Xamarin.Forms.Platform.GTK/Renderers/AbstractPageRenderer.cs
+++ Xamarin.Forms.Platform.GTK/Renderers/AbstractPageRenderer.cs
@@ -215,6 +215,9 @@ namespace Xamarin.Forms.Platform.GTK.Renderers
 
         protected virtual void SetPageSize(int width, int height)
         {
+            if (Page != null && NavigationPage.GetHasNavigationBar(Page))
+                height = height - GtkToolbarConstants.ToolbarHeight;
+
             var pageContentSize = new Gdk.Rectangle(0, 0, width, height);
             SetElementSize(pageContentSize.ToSize());
         }
