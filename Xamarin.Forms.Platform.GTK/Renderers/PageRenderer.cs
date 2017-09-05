679eed9b3be4041cf99f18f96811a3e067c320bb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-09-05 11:20:37 +0200

    Fixed StackOverflowException on CarouselPage

--- Xamarin.Forms.Platform.GTK/Renderers/PageRenderer.cs
+++ Xamarin.Forms.Platform.GTK/Renderers/PageRenderer.cs
@@ -27,7 +27,6 @@ namespace Xamarin.Forms.Platform.GTK.Renderers
             }
 
             _packager.Load();
-            FormsWindow.MainWindow.ShowAll();
         }
 
         protected override void OnSizeAllocated(Gdk.Rectangle allocation)
