8a8b906394d71c8ae2f5765892797c9c26948f99
Andoni Morales Alastruey <ylatuya@gmail.com>
2018-09-09 20:10:15 +0200

    Fix indentation

--- Xamarin.Forms.Platform.GTK/Renderers/EditorRenderer.cs
+++ Xamarin.Forms.Platform.GTK/Renderers/EditorRenderer.cs
@@ -8,158 +8,158 @@ using Xamarin.Forms.Platform.GTK.Helpers;
 
 namespace Xamarin.Forms.Platform.GTK.Renderers
 {
-    public class EditorRenderer : ViewRenderer<Editor, ScrolledTextView>
-    {
-        private const string TextColorTagName = "text-color";
+	public class EditorRenderer : ViewRenderer<Editor, ScrolledTextView>
+	{
+		private const string TextColorTagName = "text-color";
 
-        private bool _disposed;
+		private bool _disposed;
 
-        protected IEditorController EditorController => Element as IEditorController;
+		protected IEditorController EditorController => Element as IEditorController;
 
-        protected override void UpdateBackgroundColor()
-        {
-            if (!Element.BackgroundColor.IsDefaultOrTransparent())
-            {
-                var color = Element.BackgroundColor.ToGtkColor();
+		protected override void UpdateBackgroundColor()
+		{
+			if (!Element.BackgroundColor.IsDefaultOrTransparent())
+			{
+				var color = Element.BackgroundColor.ToGtkColor();
 
-                Control.TextView.ModifyBg(StateType.Normal, color);
-                Control.TextView.ModifyBase(StateType.Normal, color);
-            }
-        }
+				Control.TextView.ModifyBg(StateType.Normal, color);
+				Control.TextView.ModifyBase(StateType.Normal, color);
+			}
+		}
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
-        {
-            if (Control == null)
-            {
-                var scrolled = new ScrolledTextView();
+		protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+		{
+			if (Control == null)
+			{
+				var scrolled = new ScrolledTextView();
 
-                scrolled.TextView.Buffer.TagTable.Add(new TextTag(TextColorTagName));
-                scrolled.TextView.Buffer.Changed += TextViewBufferChanged;
-                scrolled.TextView.Focused += TextViewFocused;
-                scrolled.TextView.FocusOutEvent += TextViewFocusedOut;
+				scrolled.TextView.Buffer.TagTable.Add(new TextTag(TextColorTagName));
+				scrolled.TextView.Buffer.Changed += TextViewBufferChanged;
+				scrolled.TextView.Focused += TextViewFocused;
+				scrolled.TextView.FocusOutEvent += TextViewFocusedOut;
 
-                SetNativeControl(scrolled);
-                AdjustMinimumHeight(scrolled.TextView);
-            }
+				SetNativeControl(scrolled);
+				AdjustMinimumHeight(scrolled.TextView);
+			}
 
-            if (e.NewElement != null)
-            {
-                UpdateText();
-                UpdateFont();
-                UpdateTextColor();
-                UpdateMaxLength();
-            }
+			if (e.NewElement != null)
+			{
+				UpdateText();
+				UpdateFont();
+				UpdateTextColor();
+				UpdateMaxLength();
+			}
 
-            base.OnElementChanged(e);
-        }
+			base.OnElementChanged(e);
+		}
 
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
 
-            if (e.PropertyName == Editor.TextProperty.PropertyName)
-                UpdateText();
-            else if (e.PropertyName == Editor.TextColorProperty.PropertyName)
-                UpdateTextColor();
-            else if (e.PropertyName == Editor.FontAttributesProperty.PropertyName)
-                UpdateFont();
-            else if (e.PropertyName == Editor.FontFamilyProperty.PropertyName)
-                UpdateFont();
-            else if (e.PropertyName == Editor.FontSizeProperty.PropertyName)
-                UpdateFont();
-            else if (e.PropertyName == InputView.MaxLengthProperty.PropertyName)
-                UpdateMaxLength();
-        }
+			if (e.PropertyName == Editor.TextProperty.PropertyName)
+				UpdateText();
+			else if (e.PropertyName == Editor.TextColorProperty.PropertyName)
+				UpdateTextColor();
+			else if (e.PropertyName == Editor.FontAttributesProperty.PropertyName)
+				UpdateFont();
+			else if (e.PropertyName == Editor.FontFamilyProperty.PropertyName)
+				UpdateFont();
+			else if (e.PropertyName == Editor.FontSizeProperty.PropertyName)
+				UpdateFont();
+			else if (e.PropertyName == InputView.MaxLengthProperty.PropertyName)
+				UpdateMaxLength();
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && !_disposed)
-            {
-                _disposed = true;
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && !_disposed)
+			{
+				_disposed = true;
 
-                if (Control != null)
-                {
-                    Control.TextView.Buffer.Changed -= TextViewBufferChanged;
-                    Control.TextView.Focused -= TextViewFocused;
-                    Control.TextView.FocusOutEvent += TextViewFocusedOut;
-                }
-            }
+				if (Control != null)
+				{
+					Control.TextView.Buffer.Changed -= TextViewBufferChanged;
+					Control.TextView.Focused -= TextViewFocused;
+					Control.TextView.FocusOutEvent += TextViewFocusedOut;
+				}
+			}
 
-            base.Dispose(disposing);
-        }
+			base.Dispose(disposing);
+		}
 
-        private void UpdateText()
-        {
-            TextBuffer buffer = Control.TextView.Buffer;
+		private void UpdateText()
+		{
+			TextBuffer buffer = Control.TextView.Buffer;
 
-            if (buffer.Text != Element.Text)
-            {
-                buffer.Text = Element.Text ?? string.Empty;
-                UpdateTextColor();
-            }
-        }
+			if (buffer.Text != Element.Text)
+			{
+				buffer.Text = Element.Text ?? string.Empty;
+				UpdateTextColor();
+			}
+		}
 
-        private void UpdateFont()
-        {
-            FontDescription fontDescription = FontDescriptionHelper.CreateFontDescription(
-                Element.FontSize, Element.FontFamily, Element.FontAttributes);
-            Control.TextView.ModifyFont(fontDescription);
+		private void UpdateFont()
+		{
+			FontDescription fontDescription = FontDescriptionHelper.CreateFontDescription(
+				Element.FontSize, Element.FontFamily, Element.FontAttributes);
+			Control.TextView.ModifyFont(fontDescription);
 
-            AdjustMinimumHeight(Control.TextView, fontDescription);
-        }
+			AdjustMinimumHeight(Control.TextView, fontDescription);
+		}
 
-        private void UpdateTextColor()
-        {
-            if (!Element.TextColor.IsDefaultOrTransparent())
-            {
-                var textColor = Element.TextColor.ToGtkColor();
+		private void UpdateTextColor()
+		{
+			if (!Element.TextColor.IsDefaultOrTransparent())
+			{
+				var textColor = Element.TextColor.ToGtkColor();
 
-                TextBuffer buffer = Control.TextView.Buffer;
-                TextTag tag = buffer.TagTable.Lookup(TextColorTagName);
-                tag.ForegroundGdk = Element.IsEnabled ? textColor : Control.Style.Foregrounds[(int)StateType.Normal];
-                Control.TextView.Buffer.ApplyTag(tag, buffer.StartIter, buffer.EndIter);
-            }
-        }
+				TextBuffer buffer = Control.TextView.Buffer;
+				TextTag tag = buffer.TagTable.Lookup(TextColorTagName);
+				tag.ForegroundGdk = Element.IsEnabled ? textColor : Control.Style.Foregrounds[(int)StateType.Normal];
+				Control.TextView.Buffer.ApplyTag(tag, buffer.StartIter, buffer.EndIter);
+			}
+		}
 
-        private void TextViewBufferChanged(object sender, EventArgs e)
-        {
-            TextBuffer buffer = Control.TextView.Buffer;
+		private void TextViewBufferChanged(object sender, EventArgs e)
+		{
+			TextBuffer buffer = Control.TextView.Buffer;
 
-            if (Element.Text != buffer.Text)
-                ElementController.SetValueFromRenderer(Editor.TextProperty, buffer.Text);
+			if (Element.Text != buffer.Text)
+				ElementController.SetValueFromRenderer(Editor.TextProperty, buffer.Text);
 
-            UpdateTextColor();
-        }
+			UpdateTextColor();
+		}
 
-        private void TextViewFocused(object o, FocusedArgs args)
-        {
-            ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, true);
-        }
+		private void TextViewFocused(object o, FocusedArgs args)
+		{
+			ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, true);
+		}
 
-        private void TextViewFocusedOut(object o, FocusOutEventArgs args)
-        {
-            ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, false);
-            EditorController.SendCompleted();
-        }
+		private void TextViewFocusedOut(object o, FocusOutEventArgs args)
+		{
+			ElementController.SetValueFromRenderer(VisualElement.IsFocusedPropertyKey, false);
+			EditorController.SendCompleted();
+		}
 
-        private static void AdjustMinimumHeight(TextView textView, FontDescription font = null)
-        {
-            var fDescr = font != null ? font : textView.Style.FontDescription;
-            var metrics = textView.PangoContext.GetMetrics(font, Language.Default);
-            var pangoUnits = (metrics.Ascent + metrics.Descent) / Pango.Scale.PangoScale;
+		private static void AdjustMinimumHeight(TextView textView, FontDescription font = null)
+		{
+			var fDescr = font != null ? font : textView.Style.FontDescription;
+			var metrics = textView.PangoContext.GetMetrics(font, Language.Default);
+			var pangoUnits = (metrics.Ascent + metrics.Descent) / Pango.Scale.PangoScale;
 
-            var resolution = textView.Screen.Resolution;
-            var minHeight = (int)(pangoUnits * (resolution / 72.0));
+			var resolution = textView.Screen.Resolution;
+			var minHeight = (int)(pangoUnits * (resolution / 72.0));
 
-            if (textView.HeightRequest < minHeight)
-            {
-                textView.HeightRequest = minHeight;
-            }
-        }
+			if (textView.HeightRequest < minHeight)
+			{
+				textView.HeightRequest = minHeight;
+			}
+		}
 
-        private void UpdateMaxLength()
-        {
-            Control.SetMaxLength(Element.MaxLength);
-        }
-    }
+		private void UpdateMaxLength()
+		{
+			Control.SetMaxLength(Element.MaxLength);
+		}
+	}
 }
